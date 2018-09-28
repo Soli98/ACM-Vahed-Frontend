b9a9b33556be4a511ff6f7c3be7e7b704aec4976
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2018-09-28 17:38:15 +0330

    setup redux and material-ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+import Theme from './style/themes/theme.js';
+
+import { create } from 'jss';
+import rtl from 'jss-rtl';
+import JssProvider from 'react-jss/lib/JssProvider';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(thunk))(createStore);
+
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+const generateClassName = createGenerateClassName();
+
+ReactDOM.render(<JssProvider jss={jss} generateClassName={generateClassName}>
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <MuiThemeProvider theme={createMuiTheme(Theme)}>
+        <App />
+      </MuiThemeProvider>
+    </Provider>
+  </JssProvider>
+  , document.getElementById('root'));
 registerServiceWorker();
---
+++ src/style/themes/theme.js
@@ -0,0 +1,11 @@
+import indigo from '@material-ui/core/colors/indigo';
+
+const Theme = {
+    direction: 'rtl',
+    palette: {
+        type: 'light',
+        primary: indigo,
+    },
+}
+
+export default Theme;
