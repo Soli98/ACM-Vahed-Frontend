0dc308b4259ac225a2b4b114a170c03b43c8bf1a
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2018-10-05 16:42:08 +0330

    add plans style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/login';
 import Signup from './components/signup';
+import Plans from './components/plans';
 
 class App extends Component {
   render() {
@@ -10,7 +11,7 @@ class App extends Component {
         <Switch>
           <Route path="/login" exact component={Login} />
           <Route path="/signup" exact component={Signup} />
-          <Route path="/" exact component={Login} />
+          <Route path="/" exact component={Plans} />
         </Switch>
       </BrowserRouter>
     );
